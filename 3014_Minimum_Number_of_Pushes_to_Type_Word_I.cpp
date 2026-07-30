9d6e29994021d854798e8e6f3f3549a160d73faf
Ri_Rii <88502027+RishabhRaj120@users.noreply.github.com>
2026-07-30 21:08:46 +0530

    Solution

---
+++ 3014_Minimum_Number_of_Pushes_to_Type_Word_I.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int minimumPushes(string word)
+    {
+        vector<int> v1(26, 0);
+        for (int i = 0; i < (int)word.length(); i++)
+        {
+            ++v1[word[i] - 'a'];
+        }
+        sort(v1.begin(), v1.end(), greater<int>());
+        int c1 = 0;
+        for (int i = 0; i < 26; i++)
+        {
+            if (v1[i] == 0)
+            {
+                break;
+            }
+            else
+            {
+                c1 += ((i / 8) + 1) * v1[i];
+            }
+        }
+        return c1;
+    }
+};
