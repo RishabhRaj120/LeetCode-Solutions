51c396a2833ef1f66dcf7696028f2f4af96bf896
Ri_Rii <88502027+RishabhRaj120@users.noreply.github.com>
2026-08-31 23:45:50 +0530

    Solution

--- 3_Longest_Substring_Without_Repeating_Characters.cpp
+++ 3_Longest_Substring_Without_Repeating_Characters.cpp
@@ -9,15 +9,18 @@ public:
         int maxi = 0;
         int l = 0;
         int r = 0;
-        map<char, int> v1;
+        map<char, int> v1; // we will now store character's last index
+        for (int i = 0; i < 256; i++)
+        {
+            v1[i] = -1;
+        }
         while (r < n)
         {
-            while (v1[s.at(r)] > 0)
+            if (v1[s.at(r)] != -1)
             {
-                --v1[s.at(l)];
-                ++l;
+                l = max(l, v1[s.at(r)] + 1);
             }
-            ++v1[s.at(r)];
+            v1[s.at(r)] = r;
             maxi = max(maxi, r - l + 1);
             ++r;
         }
